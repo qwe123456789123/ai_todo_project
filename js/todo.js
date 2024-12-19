0d8a8c8a58e4d5e8406873db3a3307f655111748
qwe123456789123 <p1199019@naver.com>
2024-12-19 12:55:57 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -3,14 +3,18 @@ const todo = {
   // 작업 등록
   add(title, description, deadline) {
     const seq = Date.now();
-    this.items.push({ seq, title, description, done: false });
+    this.items.push({ seq, title, description, deadline, done: false });
+
+    this.render(); // 화면 갱신
   },
   // 작업 삭제
   remove(seq) {
     // seq로 작업 목록 순서 번호(index) 조회
     const index = this.items.findIndex((item) => item.seq === seq);
+
     // splice로 해당 순서 번호 항목 제거
     this.items.splice(index, 1);
+
     // 화면 갱신
     this.render();
   },
@@ -26,27 +30,35 @@ window.addEventListener("DOMContentLoaded", function () {
     /**
      * 1. 필수 항목 검증
      * 2. 일정 추가
+     *
      */
-    // 1. 필수 항목 검증 S
-    const requiredFields = {
-      title: "작업 제목을 입력하세요",
-      deadline: "마감일을 입력하세요",
-      description: "작업 내용을 입력하세요",
-    };
-    for (const [field, message] of Object.entries(requiredFields)) {
-      const value = frmTodo[field].value.trim();
-      if (!value) {
-        throw new Error({ field, message });
-      }
-    }
+    try {
+      // 1. 필수 항목 검증 S
+      const requiredFields = {
+        title: "작업 제목을 입력하세요.",
+        deadline: "마감일을 입력하세요.",
+        description: "작업 내용을 입력하세요.",
+      };
 
-    // 1. 필수 검증 항목 E
-    const { field, message } = err.message;
-    const el = document.getElementById(`error-${field}`);
-    if (el) {
-      el.innerText = message;
-      el.classList.remove("dn");
-      el.focus();
+      for (const [field, message] of Object.entries(requiredFields)) {
+        const value = frmTodo[field].value.trim();
+        if (!value) {
+          throw new Error(JSON.stringify({ field, message }));
+        }
+      }
+
+      // 1. 필수 항목 검증 E
+    } catch (err) {
+      const { field, message } = JSON.parse(err.message);
+      const el = document.getElementById(`error-${field}`);
+
+      console.log(err.message, el);
+
+      if (el) {
+        el.innerText = message;
+        el.classList.remove("dn");
+        el.focus();
+      }
     }
   });
 });
